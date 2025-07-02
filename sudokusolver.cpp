53d932c90d026fca925fc0c260db463fc95b17d4
Sudheer-18 <22p31a0424@acet.ac.in>
2025-07-02 13:49:08 +0530

    sudokusolver

--- sudokusolver.cpp
+++ sudokusolver.cpp
@@ -1,9 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    // map<char, int> mp; 
-  class Solution {
-public:
-    // map<char, int> mp; 
     bool Grid_Valid(vector<vector<char>>& board, int s, int e, char val) {
         for (int i = s; i < s + 3; i++) {
             for (int j = e; j < e + 3; j++) {
@@ -12,60 +12,57 @@ public:
         }
         return true;
     }
+
     bool Valid(int s, int e, vector<vector<char>>& board, char val) {
         for (int i = 0; i < 9; i++) {
             if (board[s][i] == val) return false;
-        } 
-        for (int i = 0; i < 9; i++) {
             if (board[i][e] == val) return false;
         }
-        if (!Grid_Valid(board, s - s % 3, e - e % 3, val)) return false;
+        return Grid_Valid(board, s - s % 3, e - e % 3, val);
+    }
+
+    bool solveSudokuHelper(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char val = '1'; val <= '9'; val++) {
+                        if (Valid(i, j, board, val)) {
+                            board[i][j] = val;
+                            if (solveSudokuHelper(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
-    bool solveSudokuHelper(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char val = '1'; val <= '9'; val++) {
-                        if (Valid(i, j, board, val)) {
-                            board[i][j] = val; 
-                            if (solveSudokuHelper(board)) return true;
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;  
-                }
-            }
-        }
-        return true;  
-    }
-
     void solveSudoku(vector<vector<char>>& board) {
         solveSudokuHelper(board);
     }
 };
 
-
-    bool solveSudokuHelper(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char val = '1'; val <= '9'; val++) {
-                        if (Valid(i, j, board, val)) {
-                            board[i][j] = val; 
-                            if (solveSudokuHelper(board)) return true;
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;  
-                }
-            }
+int main() {
+    vector<vector<char>> board(9, vector<char>(9));
+    for (int i = 0; i < 9; i++) {
+        string row;
+        cin >> row;
+        for (int j = 0; j < 9; j++) {
+            board[i][j] = row[j];
         }
-        return true;  
     }
 
-    void solveSudoku(vector<vector<char>>& board) {
-        solveSudokuHelper(board);
+    Solution obj;
+    obj.solveSudoku(board);
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j];
+        }
+        cout << endl;
     }
-};
+
+    return 0;
+}
